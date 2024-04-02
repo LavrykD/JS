c6e12034f3cb03b15b656b5bf6f968e63c4bf245
Dmytro Lavryk <62079347+LavrykD@users.noreply.github.com>
2024-04-02 10:24:13 +0300

    Create the HW4 folder
    Created folder contain all exercises for "Homework 4"

---
+++ HW4/Exercise_2.js
@@ -0,0 +1,4 @@
+let row_num = 8;
+for (let i = 0; i < row_num; i++) {
+    console.log(' '.repeat(row_num - i - 1) + '*'.repeat(2 * i + 1))
+}
---
+++ HW4/Exercise_3.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = 100;
+
+[a, b] = [b, a];
+
+console.log(a); // 100
+console.log(b); // 10
