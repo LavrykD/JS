a3d7832ec164a32d82091628b0e7fa5f2bc63989
Dmytro Lavryk <62079347+LavrykD@users.noreply.github.com>
2024-04-24 19:34:33 +0300

    Update app.js
    Rewrite #3, add #1 and #4; #2 in progress

--- HW10/app.js
+++ HW10/app.js
@@ -1,3 +1,28 @@
+//1
+let ivan = { name: "Іван", age: 25 };
+let petro = { name: "Петро", age: 30 };
+let mariya = { name: "Марія", age: 28 };
+let array = [ petro, ivan, mariya ];
+
+function sortByAge(arr) {
+    return array.toSorted((a, b) => a.age - b.age);
+}
+
+sortByAge(array);
+
+//2
+let users = [
+    {id: 'іван', name: "Іван Іванко", age: 20},
+    {id: 'ганна', name: "Ганна Іванко", age: 24},
+    {id: 'петро', name: "Петро Петренко", age: 31},
+];
+
+function groupById(arr) {
+    // return arr.reduce((a, c) => a[arr[i].id] = , {})
+}
+
+groupById(users);
+
 //3
 const people = [
     { name: 'Oleksii', city: 'Kyiv' },
@@ -5,13 +30,24 @@ const people = [
     { name: 'Nastia', city: 'Kyiv' },
 ];
 
-let result = people.reduce((a, c) => a[c['city']] === undefined ? a[c['city']] = [c['name']] : a[c['city']].push(c['name']), {});
-console.log(result); // ['Nastia']
+let result = people.reduce((a, c) => {
+    if (a[c.city] === undefined) {
+        a[c.city] = [c.name];    
+    } else {
+        a[c.city].push(c.name);
+    }
+    return a;
+}, {});
 
-
-result = {};
-for (let i of people) {
-    if (result[i['city']] === undefined) result[i['city']] = [i['name']];
-    else result[i['city']].push(i['name'])
-}
 console.log(result); // {'Kyiv': ['Oleksii', 'Nastia'], 'Lviv': ['Simona']}
+
+//4
+let obj = { banana: 1, orange: 2, meat: 4 };
+const doublePrices = (obj) => {
+    for (let k in obj) {
+        obj[k] *= 2;
+    }
+    return obj;
+};
+
+console.log(doublePrices(obj));
